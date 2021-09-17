73974259d71c900856fbd3a28eed07aa232a8a99
LianaChuprina <liana.chuprina201313@gmail.com>
2021-09-17 16:22:09 +0300

    no message

--- test/GUI/Main/ViewedVC/ViewedViewController.swift
+++ test/GUI/Main/ViewedVC/ViewedViewController.swift
@@ -8,30 +8,40 @@
 import UIKit
 import Moya
 
-class ViewedViewController: UIViewController {
-
+final class ViewedViewController: UIViewController {
+    
     let providerViewed = MoyaProvider<Articles>()
-   
+    
     var articles = [ArticleResponse]()
     
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            tableView.delegate = self
+            tableView.dataSource = self
+            tableView.register(
+                UINib(nibName: "\(ArticlTableViewCell.self)", bundle: nil),
+                forCellReuseIdentifier: "\(ArticlTableViewCell.self)"
+            )
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Most Viewed"
         
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(UINib(nibName: "ArticlTableViewCell",
-                                       bundle: nil),
-                                 forCellReuseIdentifier: "ArticlTableViewCell")
-        
+        fetchViewedFeed()
+    }
+    
+    private func fetchViewedFeed() {
         providerViewed.request(.viewed) { result in
             print(result)
             switch result {
             case .success(let response):
                 let articlsResponse = try? response.map(ArticlesRespose.self)
                 self.articles = articlsResponse?.results ?? []
-                self.articles.sort {$0.updated.convertToDate()!.compare($1.updated.convertToDate()!) == .orderedDescending}
+                self.articles.sort {
+                    $0.updated.convertToDate()!.compare($1.updated.convertToDate()!) == .orderedDescending
+                }
                 self.tableView.reloadData()
             case .failure(let error):
                 print(error.errorCode)
@@ -41,72 +51,62 @@ class ViewedViewController: UIViewController {
     }
 }
 
-extension ViewedViewController: UITableViewDelegate,
-                                UITableViewDataSource {
-    func tableView(_ tableView: UITableView,
-                   numberOfRowsInSection section: Int) -> Int {
+extension ViewedViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return articles.count
     }
     
-    func tableView(_ tableView: UITableView,
-                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ArticlTableViewCell",
-                                                 for: indexPath)
-        if let cellGood = cell as? ArticlTableViewCell {
-            
-            if articles[indexPath.row].media.count != 0 {
-                
-                cellGood.render(
-                    with: ArticlModel(
-                        title: articles[indexPath.row].title,
-                        time: articles[indexPath.row].updated,
-                        abstract: nil,
-                        imageURL: articles[indexPath.row].media[0].mediaMetadata[2].url,
-                        id: articles[indexPath.row].id,
-                        state: .favorite, url: articles[indexPath.row].url))}
-        else {
-            cellGood.render(with:
-                        ArticlModel(title: articles[indexPath.row].title,
-                        time:articles[indexPath.row].updated,
-                        abstract: nil, imageURL: "",
-                        id: articles[indexPath.row].id,
-                        state: .favorite, url: articles[indexPath.row].url))
-        }
-            return cellGood
-        } else {
-            return UITableViewCell()
-        }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticlTableViewCell", for: indexPath) as? ArticlTableViewCell
+        else { return UITableViewCell() }
+        
+        let imageUrl = (articles[indexPath.row].media.count > 0)
+            ? articles[indexPath.row].media[0].mediaMetadata[2].url
+            : nil
+        
+        cell.render(
+            with: ArticlModel(
+                title: articles[indexPath.row].title,
+                time: articles[indexPath.row].updated,
+                abstract: nil,
+                imageURL: imageUrl,
+                id: articles[indexPath.row].id,
+                state: .favorite, url: articles[indexPath.row].url
+            )
+        )
+        
+        return cell
     }
     
-    func tableView(_ tableView: UITableView,
-                   didSelectRowAt indexPath: IndexPath) {
-
-       let newViewController =  ArticlesVC()
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        self.present(newViewController,
-                     animated: true,
-                     completion: nil)
-        newViewController.render(model: ArticlModel(
-                                    title: articles[indexPath.row].title,
-                                    time: articles[indexPath.row].updated,
-                                    abstract: articles[indexPath.row].abstract,
-                                    imageURL:articles[indexPath.row].media[0].mediaMetadata[2].url,
-                                    id: articles[indexPath.row].id,
-                                    state: .favorite,
-                                    url: articles[indexPath.row].url
+        let newViewController =  ArticlesVC()
+        
+        self.present(newViewController, animated: true, completion: nil)
+        
+        newViewController.render(
+            model: ArticlModel(
+                title: articles[indexPath.row].title,
+                time: articles[indexPath.row].updated,
+                abstract: articles[indexPath.row].abstract,
+                imageURL:articles[indexPath.row].media[0].mediaMetadata[2].url,
+                id: articles[indexPath.row].id,
+                state: .favorite,
+                url: articles[indexPath.row].url
+            )
         )
-    )
-}
-
+    }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return Constants.cellHeight
     }
 }
 
-    extension ViewedViewController {
-        private enum Constants {
-    static let cellHeight: CGFloat = 150.0
-        }
+extension ViewedViewController {
+    private enum Constants {
+        static let cellHeight: CGFloat = 150.0
+    }
 }
 
 
